97b0c192a3697bf6ef656c04d95d24e23074ff89
Sark <kosinick01@gmail.com>
2023-07-27 02:41:35 +0000

    added descriptions

--- main.h
+++ main.h
@@ -92,116 +92,116 @@ typedef struct _builtin_
 	int (*f)(datashell *data_sh);
 } _builtin;
 
-/* aux_lists.c */
+/* lists_aux.c */
 list_sep *add_sep_end(list_sep **head, char sep);
 void free_list_sep(list_sep **head);
 list_line *add_line_end(list_line **head, char *line);
 void free_list_line(list_line **head);
 
-/* aux_lists2.c */
+/* lists2_aux.c */
 var_li *_add_rvar_node_end(var_li **head, int lvar, char *var, int lval);
 void free_var_li(var_li **head);
 
-/* aux_str functions */
+/* str_aux functions */
 char *_strcat(char *dest, const char *src);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 
-/* aux_mem.c */
+/* mem_aux.c */
 void _memcpy(void *newpointr, const void *pointr, unsigned int size);
 void *_realloc(void *pointr, unsigned int old_len, unsigned int new_len);
 char **_realloc_dp(char **pointr, unsigned int old_len, unsigned int new_len);
 
-/* aux_str2.c */
+/* str2_aux.c */
 char *_strdup(const char *s);
 int _strlen(const char *s);
 int cmp_char_len(char arr[], const char *delim);
 char *_strtok(char arr[], const char *delim);
 int _isdigit(const char *s);
 
-/* aux_str3.c */
+/* str3_aux.c */
 void rev_string(char *s);
 
-/* check_syntax_error.c */
+/* error_check_syntax.c */
 int num_repeated_char(char *input, int index);
 int error_snytx(char *input, int index, char last);
 int indx_first_char(char *input, int *index);
 void print_syntx_err(datashell *data_sh, char *input, int i, int bool);
 int check_syntx_error(datashell *data_sh, char *input);
 
-/* shell_loop.c */
+/* loop_shell.c */
 char *_without_comment(char *input);
 void _shell_loop(datashell *data_sh);
 
-/* read_line.c */
+/* line_read.c */
 char *_read_line(int *line_val);
 
-/* split.c */
+/* _split.c */
 char *_swap_char(char *input, int bool);
 void _add_nodes(list_sep **head_s, list_line **head_l, char *input);
 void _go_next(list_sep **list_s, list_line **list_l, datashell *data_sh);
 int _split_commands(datashell *data_sh, char *input);
 char **_split_line(char *input);
 
-/* rep_var.c */
+/* var_rep.c */
 void _check_env(var_li **h, char *in, datashell *data);
 int _check_vars(var_li **h, char *in, char *st, datashell *data);
 char *_replaced_input(var_li **head, char *input, char *new_input, int nlen);
 char *_replace_var(char *input, datashell *data_sh);
 
-/* get_line.c */
+/* line_get.c */
 void _bring_line(char **lineptr, size_t *n, char *buffer, size_t j);
 ssize_t _get_line(char **lineptr, size_t *n, FILE *stream);
 
-/* exec_line */
+/* line_exec */
 int _exec_line(datashell *data_sh);
 
-/* cmd_exec.c */
+/* exec_cmd.c */
 int _is_cdir(char *path, int *i);
 char *_which_(char *cmd, char **_environ);
 int _is_executable(datashell *data_sh);
 int check_err_cmd(char *dir, datashell *data_sh);
 int _cmd_exec(datashell *data_sh);
 
-/* env1.c */
+/* env_1.c */
 char *_getenv(const char *name, char **_environ);
 int _env(datashell *data_sh);
 
-/* env2.c */
+/* env_2.c */
 char *copy_info(char *name, char *value);
 void _set_env(char *name, char *value, datashell *data_sh);
 int _setenv(datashell *data_sh);
 int _unsetenv(datashell *data_sh);
 
-/* cd.c */
+/* _cd.c */
 void cd_paren(datashell *data_sh);
 void cd_given(datashell *data_sh);
 void cd_prev_dir(datashell *data_sh);
 void cd_to_home_dir(datashell *data_sh);
 
-/* cd_shell.c */
+/* shell_cd.c */
 int _cd_shell(datashell *data_sh);
 
-/* get_builtin */
+/* builtin_get */
 int (*_get_builtin(char *cmd))(datashell *data_sh);
 
-/* _exit.c */
+/* exit_shell.c */
 int _exit_shell(datashell *data_sh);
 
-/* aux_stdlib.c */
+/* stdlib_aux.c */
 int get_length(int n);
 char *_itoa_(int n);
 int _atoi_(char *s);
 
-/* aux_err1.c */
+/* err1_aux.c */
 char *strcat_cd_er(datashell *, char *, char *, char *);
 char *error_msg_get_cd(datashell *data_sh);
 char *error_cmd_not_found(datashell *data_sh);
 char *err_exit_shell(datashell *data_sh);
 
-/* aux_err2.c */
+/* err2_aux.c */
 char *error_get_alias(char **args);
 char *error_msg_env(datashell *data_sh);
 char *error_syntax(char **args);
@@ -209,25 +209,25 @@ char *error_permission(char **args);
 char *error_msg_path(datashell *data_sh);
 
 
-/* get_err.c */
+/* err_get.c */
 int _get_err(datashell *data_sh, int err_val);
 
-/* handle_sig.c */
+/* sig_handle.c */
 void _get_sigint(int signal);
 
-/* aux_help.c */
+/* help_aux.c */
 void aux_help_env_info(void);
 void aux_help_setenv_info(void);
 void aux_help_unsetenv_info(void);
 void aux_help_general_info(void);
 void aux_help_exit_info(void);
 
-/* aux_help2.c */
+/* help2_aux.c */
 void aux_help_info(void);
 void aux_help_alias_info(void);
 void aux_help_cd_info(void);
 
-/* get_help.c */
+/* help_get.c */
 int _get_help(datashell *data_sh);
 
 #endif
